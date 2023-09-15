2149f5accd69ab5d514382fcfe42d12881211a58
Kristof-Toth <tothkristof98@gmail.com>
2023-09-15 11:19:03 +0200

    change quantity

--- src/main/java/com/library/springbootlibrary/service/AdminService.java
+++ src/main/java/com/library/springbootlibrary/service/AdminService.java
@@ -1,17 +1,49 @@
 package com.library.springbootlibrary.service;
 
 import com.library.springbootlibrary.dao.BookRepository;
+import com.library.springbootlibrary.dao.CheckoutRepository;
+import com.library.springbootlibrary.dao.ReviewRepository;
 import com.library.springbootlibrary.entity.Book;
 import com.library.springbootlibrary.requestmodels.AddBookRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AdminService {
     private final BookRepository bookRepository;
+    private final ReviewRepository reviewRepository;
+    private final CheckoutRepository checkoutRepository;
+
+    public void increaseBookQuantity(Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if (!book.isPresent())
+            throw new Exception("Book was not found");
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+        book.get().setCopies(book.get().getCopies() + 1);
+
+        bookRepository.save(book.get());
+    }
+
+    public void decreaseBookQuantity(Long bookId) throws Exception {
+
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if (!book.isPresent() || book.get().getCopiesAvailable() <= 0 || book.get().getCopies() <= 0) {
+            throw new Exception("Book not found or quantity locked");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        book.get().setCopies(book.get().getCopies() - 1);
+
+        bookRepository.save(book.get());
+    }
 
     public void postBook(AddBookRequest addBookRequest) {
         Book book = Book.builder()
@@ -25,5 +57,18 @@ public class AdminService {
                 .build();
 
         bookRepository.save(book);
+    }
+
+    public void deleteBook(Long bookId) throws Exception {
+
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if (!book.isPresent()) {
+            throw new Exception("Book not found");
+        }
+
+        bookRepository.delete(book.get());
+        checkoutRepository.deleteAllByBookId(bookId);
+        reviewRepository.deleteAllByBookId(bookId);
     }
 }
